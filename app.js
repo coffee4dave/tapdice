a9858d4406c3c1634982b04af66bf069e8d5cbdf
Dave <lifecoachdavebrown@gmail.com>
2020-09-11 15:39:16 -0700

    first

---
+++ app.js
@@ -0,0 +1,55 @@
+const title = document.querySelector("#title");
+const body = document.querySelector("body");
+const p1 = document.querySelector("#p1");
+const p2 = document.querySelector("#p2");
+const message = document.querySelector("#message");
+
+let clicks = 0;
+
+window.onload = () => {
+  console.log("loaded");
+  console.log(p1.classList);
+};
+
+body.addEventListener("click", () => {
+  clicks++;
+  p1.classList = "";
+  p2.classList = "";
+
+  p1.classList.add("dice");
+  p1.classList.add("spin");
+  p2.classList.add("dice");
+  p2.classList.add("spin");
+
+  let num1 = randNum(1, 6);
+  console.log(num1);
+  p1.classList.add("dice" + num1);
+  let num2 = randNum(1, 6);
+  console.log(num2);
+  p2.classList.add("dice" + num2);
+
+  if (num1 > num2) {
+    message.textContent = "Player 1 Wins!";
+  } else if (num1 === num2) {
+    message.textContent = "Tie!";
+  } else {
+    message.textContent = "Player 2 Wins!";
+  }
+});
+
+function randNum(base, limit) {
+  return Math.floor(Math.random() * limit) + base;
+}
+
+function test() {
+  console.log("test start");
+  for (let i = 1; i < 100000; i++) {
+    let result = randNum(1, 6);
+    if (result < 1 || result > 6) {
+      console.log("out of range");
+    }
+  }
+  console.log("test complete");
+}
+
+// test();
